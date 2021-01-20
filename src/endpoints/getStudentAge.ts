eb67953a38c500a53aff4273e3ef7b10e7558ffd
Felipe Polato <fepolato@hotmail.com>
2021-01-20 15:45:30 -0300

    funcao calculaIdade

--- src/endpoints/getStudentAge.ts
+++ src/endpoints/getStudentAge.ts
@@ -1,6 +1,33 @@
 import { Request, Response } from 'express'
 import { selectStudentById } from '../data/selectStudentById'
 
+function calculaIdade(dataNasc: string ){ 
+    const dataAtual = new Date();
+    const anoAtual = dataAtual.getFullYear();
+    const anoNascParts = dataNasc.split('-');
+    const diaNasc =anoNascParts[0];
+    const mesNasc =anoNascParts[1];
+    const anoNasc =anoNascParts[2];
+    let idade = anoAtual - anoNasc;
+
+
+    const mesAtual = dataAtual.getMonth() + 1; 
+    //Se mes atual for menor que o nascimento, nao fez aniversario ainda;  
+    if(mesAtual < mesNasc){
+    idade--; 
+    } else {
+    //Se estiver no mes do nascimento, verificar o dia
+    if(mesAtual == mesNasc){ 
+    if(new Date().getDate() < diaNasc ){ 
+    //Se a data atual for menor que o dia de nascimento ele ainda nao fez aniversario
+    idade--; 
+    }
+    }
+    } 
+    return idade; 
+   }
+   console.log(calculaIdade('31/12/1970'));
+
 export const getStudentAge = async (req: Request, res: Response): Promise<any> => {
     try {
         const id = Number(req.params.id);
@@ -16,3 +43,4 @@ export const getStudentAge = async (req: Request, res: Response): Promise<any> =
     }
 
 }
+
