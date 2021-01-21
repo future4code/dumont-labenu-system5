09e94a86a5efcd58753554cdb586b9ed4609c63b
Noh Ah Jeong <noh_ah@hotmail.com>
2021-01-21 09:43:05 -0300

    endpoint pegar idade arrumada

--- src/data/selectStudentById.ts
+++ src/data/selectStudentById.ts
@@ -3,12 +3,11 @@ import { connection } from "../index"
 export const selectStudentById = async ( id: number
     
 ): Promise<any> => {
-
     const result = await connection.raw(`
-    SELECT name, FLOOR(DATEDIFF(CURDATE(), birthdate)/365) AS age
-    FROM Student
-    WHERE id = "${id}";
-`);
+        SELECT name, FLOOR(DATEDIFF(CURDATE(), birthdate)/365) AS age
+        FROM student
+        WHERE id = "${id}";
+    `);
+
     return result[0];
 };
-
--- src/endpoints/getStudentAge.ts
+++ src/endpoints/getStudentAge.ts
@@ -1,43 +1,15 @@
 import { Request, Response } from 'express'
 import { selectStudentById } from '../data/selectStudentById'
 
-function calculaIdade(dataNasc: String){ 
-    const dataAtual = new Date();
-    const anoAtual = dataAtual.getFullYear();
-    const anoNascParts = dataNasc.split('-');
-    const diaNasc =Number(anoNascParts[0]);
-    const mesNasc =Number(anoNascParts[1]);
-    const anoNasc =Number(anoNascParts[2]);
-    let idade = anoAtual - anoNasc;
-    const mesAtual = dataAtual.getMonth() + 1; 
-
-
-    //Se mes atual for menor que o nascimento, nao fez aniversario ainda;  
-    if(mesAtual < mesNasc){
-    idade--; 
-    } else {
-    //Se estiver no mes do nascimento, verificar o dia
-    if(mesAtual == mesNasc){ 
-    if(new Date().getDate() < diaNasc ){ 
-    //Se a data atual for menor que o dia de nascimento ele ainda nao fez aniversario
-    idade--; 
-    }}}; 
-
-    return idade; 
-   };
-
-  export const getStudentAge = async (req: Request, res: Response): Promise<any> => {
+export const getStudentAge = async (req: Request, res: Response): Promise<any> => {
     try {
         const id = Number(req.params.id);
+
         const student = await selectStudentById(id);
 
-        calculaIdade(student.birthdate);
-        
-
-        console.log(calculaIdade(student.birthdate));
-
+        res.status(200).send(student);
     } catch (error) {
-        res.send(error.message)
+        res.status(400).send(error.message);
     }
 
 }
